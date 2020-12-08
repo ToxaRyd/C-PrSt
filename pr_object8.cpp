d4e7afc754cde1125ef11250127621ad7ab90af5
ToxaRyd <65728417+ToxaRyd@users.noreply.github.com>
2020-12-08 17:20:30 +0300

    Add files via upload

---
+++ pr_object8.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <utility>
+
+bool mypredicate (int i, int j) {
+  return (i==j);
+}
+
+int main () {
+  std::vector<int> myvector;
+  for (int i=1; i<6; i++) myvector.push_back (i*10); // myvector: 10 20 30 40 50
+
+  int myints[] = {10,20,80,320,1024};                //   myints: 10 20 80 320 1024
+
+  std::pair<std::vector<int>::iterator,int*> mypair;
+
+  // using default comparison:
+  mypair = std::mismatch (myvector.begin(), myvector.end(), myints);
+  std::cout << "First mismatching elements: " << *mypair.first;
+  std::cout << " and " << *mypair.second << '\n';
+
+  ++mypair.first; ++mypair.second;
+
+  // using predicate comparison:
+  mypair = std::mismatch (mypair.first, myvector.end(), mypair.second, mypredicate);
+  std::cout << "Second mismatching elements: " << *mypair.first;
+  std::cout << " and " << *mypair.second << '\n';
+
+  return 0;
+}
