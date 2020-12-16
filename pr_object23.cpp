3720075e3bf86ff44f7814b4fed2da8f01dc709c
ToxaRyd <65728417+ToxaRyd@users.noreply.github.com>
2020-12-16 21:07:43 +0200

    Add files via upload

---
+++ pr_object23.cpp
@@ -0,0 +1,13 @@
+#include <algorithm>
+#include <iostream>
+ 
+int main()
+{
+    int a = 5, b = 3;
+
+    std::cout << a << ' ' << b << '\n';
+ 
+    std::swap(a,b);
+
+    std::cout << a << ' ' << b << '\n';
+}
